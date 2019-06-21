08dbb9960661eb8b45a6df15759bb42f63b6a7dd
matsuokatoshiki <swimbackman@i.softbank.jp>
2019-06-21 10:37:42 +0900

    画像のキャッシュを追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,7 +63,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body, :image)
+      params.require(:post).permit(:title, :body, :image, :image_cache)
     end
 
 end
