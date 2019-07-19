2940ba06d9b9af6efec020e1800442bf64b17d66
matsuokatoshiki <swimbackman@i.softbank.jp>
2019-07-19 20:08:49 +0900

    testを追加しました。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :body, presence: true, length: { maximum: 200 }
+  validate :check_number_of_comments
+  def check_number_of_comments
+    if post && post.comments.count >= 10
+      errors.add(:body, "数の上限に達したのでコメントできませんでした。")
+    end
+  end
 end
